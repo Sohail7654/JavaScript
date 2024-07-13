bdb99d524f41181789ae87672163c3c6a6d0ac70
Sohail7654 <83806736+Sohail7654@users.noreply.github.com>
2024-07-13 01:01:09 +0000

    02_Variables

---
+++ Basic/02_Variables.js
@@ -0,0 +1,17 @@
+const UserId=12345
+let UserName="SohailAkter"
+var Password="12345"
+UserCity="New Delhi"
+let UserState
+
+console.log(UserId)
+
+// UserId=54321 //Can not modify constant Value
+
+//console.log(UserId); 
+/*
+    Can not use var
+    Because of issue in block and scop lavel problem
+*/
+
+console.table([UserId,UserName,Password,UserCity,UserState])  
