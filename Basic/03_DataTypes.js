8e44e83f99c7e2d3f9029bb91c5655d51abf837a
Sohail7654 <83806736+Sohail7654@users.noreply.github.com>
2024-07-13 02:41:01 +0000

    Conversion

---
+++ Basic/03_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //Treat all js code as new version
+
+let UserName="Default"
+let mobile=7654801480
+let islogedin= true
+let State;  //undefined variable, if we print this variable it will return undefined
+let Temprature=null;   
+
+// alert(5+5)   //we are using node js not browser
+
+/* Data types
+nummber=>2 to the power 53
+string=>""
+boolean=>true/false
+undefined
+null=>standalone value
+symbol
+
+
+object
+
+*/
+
+console.log(typeof UserName)
+console.log(typeof mobile)
+console.log(typeof State)
+console.log(typeof islogedin)
+console.log(typeof Temprature)
---
+++ Basic/04_DataType_Conversion.js
@@ -0,0 +1,40 @@
+const num=33
+
+//console.log(typeof num) //number
+
+let stringNum=String(num)
+// console.log(stringNum)
+// console.log(typeof stringNum)
+
+const variable1="33ab"
+let numVariable1=Number(variable1)
+// console.log(numVariable1)   //NaN
+// console.log(typeof numVariable1)   
+
+const variable2= "abcd"
+// console.log(Boolean(variable2))
+// console.log(typeof Boolean(variable2))
+
+//************************************Oprations**************************
+let str=33
+let negStr=-str
+// console.log(negStr);
+
+// console.log(3+3)
+// console.log(3-3);
+// console.log(3*3)
+// console.log(3**3);
+// console.log(3/3)
+// console.log(3%5)
+
+// console.log(1+"2") //12
+// console.log("1"+2) //12
+// console.log("1"+2+3) //123
+// console.log(1+2+"3") //33
+
+// console.log(true)//true
+// console.log(+true) // 1
+// console.log(-"") // -0
+
+let num1,num2,num3;
+num1=num2=num3=3+4 // it is correct but don't use because readability is not good
