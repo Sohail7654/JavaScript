fd84a14980c7aa3f5657bc0b2fb9cd3e017cb52b
Sohail7654 <83806736+Sohail7654@users.noreply.github.com>
2024-07-18 18:26:11 +0000

    Number and Maths

---
+++ Basic/06Number_and_Math.js
@@ -0,0 +1,25 @@
+const num=100
+// console.log(num);
+
+const num2=new Number(100);
+// console.log(num2)
+
+// console.log(num2.toFixed()) //toFixed function return a string value
+// console.log(num2.toPrecision(1))
+
+// ***************Math********************
+
+const num3=400
+console.log(Math.PI)
+console.log(Math.abs(-5))
+console.log(Math.floor(365.96))
+console.log(Math.ceil(659.01))
+console.log(Math.max(3,5,1,9,4,8))
+console.log(Math.min(5,96,4,3,7,0,5,85))
+
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min)); //it will always return min to max value
